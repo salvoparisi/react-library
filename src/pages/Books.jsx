717cbc3b5291e030b7d316b6efd907955c7595bd
salvoparisi <psalvo065@gmail.com>
2024-11-25 11:00:00 +0100

    add MainMenu

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,14 +1,11 @@
+import MainMenu from '../components/MainMenu.jsx'
+
 function Books() {
     return (
         <>
             <header>
                 <h2>Logo</h2>
-                <nav>
-                    <a href="/">Home</a>
-                    <a href="/about">About</a>
-                    <a href="/contacts">Contacts</a>
-                    <a href="/books">Books</a>
-                </nav>
+                <MainMenu />
             </header>
             <main>
                 <h1>Books Page</h1>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,11 @@
+import MainMenu from '../components/MainMenu.jsx'
+
 function Home() {
     return (
         <>
             <header>
                 <h2>Logo</h2>
-                <nav>
-                    <a href="/">Home</a>
-                    <a href="/about">About</a>
-                    <a href="/contacts">Contacts</a>
-                    <a href="/books">Books</a>
-                </nav>
+                <MainMenu />
             </header>
             <main>
                 <h1>Main Page</h1>
